7a64b427709cfa2c8c1a904d7f52acbbbe4715d4
Shamil Khalilov <shamfama@gmail.com>
2020-03-26 23:13:03 +0300

    Fix Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 FROM node:12-stretch-slim
 
-COPY package.json package-lock.json /app/
+COPY package.json package-lock.json index.js /app/
 
 WORKDIR /app
 
 COPY public /app/public
 COPY src /app/src
 
-RUN npm install
+RUN npm i
+RUN npm i express
 RUN npm run build
 
 ENTRYPOINT [ "npm", "run", "serve" ]
--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-
 const express = require('express');
 const expressApp = express();
 
-const port = grabProcessArgumentValue(process.argv, '--port') || process.env.PORT || 5000;
-const httpServer = expressApp.listen(port, () => {
-    console.log(__dirname); // eslint-disable-line no-console
-    console.log('Listening Port ' + port); // eslint-disable-line no-console
-});
+const port = process.env.PORT || 5000;
 
 expressApp.use(function(req, res, next) {
     if (req.header('x-forwarded-proto') === 'http') {
@@ -16,10 +11,13 @@ expressApp.use(function(req, res, next) {
     next();
 });
 
-mainApp.start();
-
 expressApp.use(express.static(__dirname + '/build'));
 
 expressApp.get('/*', (req, res) => {
     res.sendFile('index.html', { root: __dirname + '/build' });
+});
+
+const httpServer = expressApp.listen(port, () => {
+    console.log(__dirname); // eslint-disable-line no-console
+    console.log('Listening Port ' + port); // eslint-disable-line no-console
 });
